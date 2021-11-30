babf69292c7483c46094b9438f54a3f20c130844
filauro1989 <filauro1989@gmail.com>
2021-11-30 16:20:05 +0100

    styling

--- jsnack 2/js/script.js
+++ jsnack 2/js/script.js
@@ -1,6 +1,6 @@
 numeri = [];
 
-function oddNumber(num) {
+function somma(num) {
     let somma = 0;
 
     for (let index = 1; index < num.length; index += 2) {
@@ -11,14 +11,18 @@ function oddNumber(num) {
 
 }
 
-let randomNumber;
 let i = 0;
     while (i < 20) {
         numeri.push(Math.floor(Math.random() * 100));
         i++;
     }
 
+let numbers = document.getElementById('numbers');
+numbers.innerHTML += numeri;
 
 console.log(numeri);
-let numerodispari = oddNumber(numeri);
+let numerodispari = somma(numeri);
 console.log(numerodispari);
+
+let slot = document.querySelector('.slot');
+slot.innerHTML += numerodispari;
