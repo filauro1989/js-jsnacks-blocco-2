fb6d2e8ac557e82702dec6c356e8ac263fdf882b
filauro1989 <filauro1989@gmail.com>
2021-11-30 10:01:14 +0100

    first commit

---
+++ jsnack1/js/script.js
@@ -0,0 +1,26 @@
+nomi = [
+    'piero',
+    'luca',
+    'giacomo',
+    'andrea',
+    'mario'
+];
+
+cognomi = [
+    'merola',
+    'pacciani',
+    'forlini',
+    'sabani',
+    'pieroni'
+]
+
+function generatore(atr1, atr2) {
+
+    let nome = months[Math.floor(Math.random() * months.length)];
+
+    let persona = '';
+       
+ }
+
+nomecognome = generatore(nomi, cognomi);
+console.log(nomecognome);
