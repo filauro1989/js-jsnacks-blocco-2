afaaa9988ef380e996097eadf48cbb432ebe10b9
filauro1989 <filauro1989@gmail.com>
2021-11-30 10:04:15 +0100

    finished

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -16,11 +16,11 @@ cognomi = [
 
 function generatore(atr1, atr2) {
 
-    let nome = months[Math.floor(Math.random() * months.length)];
-
-    let persona = '';
-       
- }
+    let nome = atr1[Math.floor(Math.random() * atr1.length)];
+    let cognome = atr2[Math.floor(Math.random() * atr2.length)];
+    let persona = nome + ' ' + cognome;
+    return persona;  
+}
 
 nomecognome = generatore(nomi, cognomi);
 console.log(nomecognome);
